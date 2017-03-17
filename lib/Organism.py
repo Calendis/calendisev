1059811fc669110dbaaa089e90bb2edb2578c807
Calendis <calendis1@gmail.com>
2017-03-17 14:51:49 -0600

    Fixed animal movement bugs

--- lib/Organism.py
+++ lib/Organism.py
@@ -10,7 +10,7 @@ from lib.Name import * #Imports lists from Name.py
 import pygame #Imports pygame library
 
 from lib.Helper import nonzero #Imports functions from Helper.py
-from lib.Helper import nonzeror
+from lib.Helper import nonzerov
 from lib.Helper import alfour
 from lib.Helper import customlimit
 from lib.Helper import binaps
@@ -31,26 +31,18 @@ class Organism(): #Base class for all organisms
 		self.x = 0 #Some basic properties that an organism must have
 		self.y = 0
 		self.size = (random()*10) +1
-		self.maxspeed = nonzero(17-self.size/2)/20 +0.05
-		self.xspeed = binaps(self.maxspeed / 2)
-		self.yspeed = binaps(self.maxspeed / 2)
+		self.maxspeed = (nonzero(17-self.size/2)/20 +0.05)*2
+		self.xspeed = binaps(self.maxspeed / 4)
+		self.yspeed = binaps(self.maxspeed / 4)
 		self.xaccel = 0
 		self.yaccel = 0
-		self.hitbox = pygame.Rect(self.x, self.y, self.size, self.size)
+		self.hitbox = pygame.Rect(self.x, self.y, self.size/3+2, self.size/3+2)
 		self.maxfitness = 175 * (self.size/2)
 		self.fitness = self.maxfitness/2
 		self.dead = False
 		self.dormant = False
 		self.seedrange = randint(1,40)
 		self.poison = random()
-
-		
-		'''self.brain_inputs = []
-		self.brain_layer_0 = []
-		self.brain_layer_1 = []
-		self.brain_outputs = []
-
-		self.brain = [self.brain_inputs, self.brain_layer_0, self.brain_layer_1,self.brain_outputs]'''
 
 		self.energy = self.maxfitness*2
 		self.lifespan = round(((self.maxfitness)*self.size)*0.6)
@@ -75,10 +67,17 @@ class Organism(): #Base class for all organisms
 		self.name = name_1[randint(0,(len(name_1)-1))]+name_2[randint(0,len(name_2)-1)]+" "+name_1[randint(0,(len(name_1)-1))]+name_2[randint(0,len(name_2)-1)]
 
 	def update(self): #Function that performs basic tasks on the organism, such as moving
-		'''if self.xspeed > self.maxspeed or self.yspeed > self.maxspeed:
+		'''if abs(self.xspeed) > self.maxspeed or abs(self.yspeed) > self.maxspeed:
 			self.colour = (255,0,255)#Change to magenta if speed is malfunctioning. (Useful for debugging)
+			print("Speed error on "+str(self))
+			print(self.xspeed)
+			print(self.yspeed)
+			print(self.maxspeed)
 		else:
-			self.colour = (0,255,0)'''
+			print("Speed normal on "+str(self))
+			print(self.xspeed)
+			print(self.yspeed)
+			print(self.maxspeed)'''
 
 		self.fitness = customlimit(self.fitness, self.maxfitness)
 		self.energy = customlimit(self.energy, self.maxfitness)
@@ -87,8 +86,9 @@ class Organism(): #Base class for all organisms
 
 		self.lifespan -= 1 #An organism dies when its lifespan reaches zero
 		
-		if self.__class__ == Animal: #An organism loses energy, and loses more if it is larger
-			self.energy -= 0.05*self.size
+		if self.__class__ == Animal: #An organism loses energy, and loses more if it is larger and the closer it is to max speed
+			self.energy -= 0.05*self.size*(((self.xspeed+self.yspeed)/2)/self.maxspeed)
+			self.energy -= self.poison #It takes energy to produce poison
 		
 		if self.fitness < 1 or self.lifespan < 0:
 			self.dead = True #Kills the organism if its fitness or lifespan variables go too low
@@ -109,8 +109,9 @@ class Organism(): #Base class for all organisms
 		self.yspeed = customlimit(self.yspeed, self.maxspeed)
 
 		if self.__class__ == Animal: #Speed must never be zero if the organism is an animal
-			self.xspeed = nonzeror(self.xspeed)
-			self.yspeed = nonzeror(self.yspeed)
+			#self.xspeed = nonzero(self.xspeed)
+			#self.yspeed = nonzero(self.yspeed)
+			self.xspeed, self.yspeed = nonzerov(self.xspeed, self.yspeed)
 
 		if self.target == False: #Acceleration is set to zero if the organism has no target
 			self.xaccel = 0
@@ -128,7 +129,7 @@ class Organism(): #Base class for all organisms
 			self.x += self.xspeed #Updates the x and y positions with the speed, if the organism is alive
 			self.y += self.yspeed
 		
-		self.hitbox = pygame.Rect(self.x-self.size, self.y-self.size, self.size*2, self.size*2)
+		self.hitbox = pygame.Rect(self.x-self.size/3, self.y-self.size/3, self.size*2/3+2, self.size*2/3+2)
 		#A rectangle object based on the organism's size and position
 
 		self.view = pygame.Rect(self.x-self.seerange/2, self.y-self.seerange/2, self.seerange, self.seerange)
@@ -174,17 +175,15 @@ class Organism(): #Base class for all organisms
 			self.waterproofing = 0
 		self.waterrange = range(round(50-(6*self.waterproofing))-40,round(50-(6*self.waterproofing)))
 
-		#Mutates the seedrange and poison values if the organism is a plant...
+		#Mutates the seedrange value if the organism is a plant...
 		if self.__class__ == Plant or self.__class__ == CallablePlant:
 			self.seedrange += randint(round(-self.mutability),round(self.mutability))
-			self.poison += randint(round(-self.mutability),round(self.mutability))
+
+		#Mutates the poison value
+		self.poison += randint(round(-self.mutability),round(self.mutability))
 
 		#Mutates the mutability itself
 		self.mutability = raisefromzero(self.mutability+randint(round(-self.mutability*10),round(self.mutability*10))/10)
-
-		#Ensures that organisms are always large enough to be drawn correctly
-		if self.size < 2:
-			self.size = 2
 
 class Animal(Organism): #Class for animal
 	"""docstring for Animal"""
